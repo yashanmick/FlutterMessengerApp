d6565e476a8afdf33a1e0b40dd476f216a80b0ec
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-07-20 23:07:17 +0530

    Timer animations

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -10,11 +10,59 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  Animation animation; //curve animations
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this, //initializing the ticker
+      //upperBound: 100.0,
+    );
+
+    animation = CurvedAnimation(
+        parent: controller,
+        curve: Curves
+            .decelerate); //when applying curve to controller we cant apply upperBound > 1
+
+    //controller.forward();               //calling the controller IMP : you can also call reverse
+    //controller.reverse(from: 1.0);          //reverse animation
+
+    //forward animation
+    controller.forward();
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse(from: 1.0);
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+  }
+
+  //whenever uses a animation controller really important to add dispose
+  @override
+  void dispose() {
+    //implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      //backgroundColor: Colors.red.withOpacity(controller.value),      //using ticker animations
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -27,13 +75,15 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
-                  ),                  
+                    height: animation.value * 100, //animation value
+                    //height: controller.value, //controlling size by ticker animations
+                  ),
                 ),
                 Text(
                   'Flash Chat',
+                  //'${controller.value.toInt()}%',       //a loading indicator
                   style: TextStyle(
-                    fontSize: 45.0,
+                    fontSize: 40.0,
                     fontWeight: FontWeight.w900,
                   ),
                 ),
